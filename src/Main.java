1e94f7f18fc5e46ec4e6f682c7657f75b36d591d
Zach-Adolphsen <zladolphsen@gmail.com>
2024-11-26 18:42:54 -0600

    Just practicing

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,13 @@ public class Main {
         System.out.println("Hello, World!");
 
         System.out.println("Does this work?");
+
+        System.out.println("Just doing som practice to get used to ");
+
+        for (int i = 0; i < 7; i++){ // just random code to practice with
+            System.out.println("i = " + i);
+            if(i == 3)
+                break;
+        }
     }
 }
